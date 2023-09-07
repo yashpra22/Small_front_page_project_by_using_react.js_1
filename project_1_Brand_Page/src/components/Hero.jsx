95a70d5001cd12de414e931ead9aa7ac762dadc3
yashpra22 <143728346+yashpra22@users.noreply.github.com>
2023-09-07 13:49:13 +0530

    Add files via upload
    Use "npm install "  OR   " npm  i"  for package.jasason filee .

---
+++ project_1_Brand_Page/src/components/Hero.jsx
@@ -0,0 +1,32 @@
+const HeroSection = () => {
+    return (
+        <main className="hero">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>
+                    YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR 
+                    SHOES. YOUR FEET DESREVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR 
+                    SHOES.
+                </p>
+                <div className="hero-btn">
+                    <button>SHOP NOW</button>
+                    <button>CATEGORY</button>
+                </div >
+                <div className="shopping ">
+                    <p> ALSO AVAILABLE ON</p>   
+                </div>
+                <div className="brand-icons">
+                    <img src="/images/shops.png" alt="" />
+                </div>
+            </div>
+
+            <div className="hero-image">
+            <img src="/images/shoe_image.png"></img>
+
+
+
+            </div>
+        </main>
+    );
+};
+export default HeroSection;
